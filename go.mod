8a3dfb5f989a8fb64028725e9742e1952cddb60d
Sarthak <sarthaktomar2143@gmail.com>
2022-08-22 11:39:16 +0000

    Rewriting whole project.

--- go.mod
+++ go.mod
@@ -4,9 +4,9 @@ go 1.18
 
 replace example.com/password => ./password
 
-require (
-	example.com/password v0.0.0-00010101000000-000000000000
-	github.com/boltdb/bolt v1.3.1
-)
+require example.com/password v0.0.0-00010101000000-000000000000
 
-require golang.org/x/sys v0.0.0-20220818161305-2296e01440c6 // indirect
+require (
+	go.etcd.io/bbolt v1.3.6 // indirect
+	golang.org/x/sys v0.0.0-20220818161305-2296e01440c6 // indirect
+)
--- main.go
+++ main.go
@@ -2,44 +2,12 @@ package main
 
 import (
     "fmt"
-    "log"
+    //"log"
     "example.com/password"
-    "github.com/boltdb/bolt"
 )
 
 func main() {
-    err := password.Put_pwd("Test", "Pa55word")
-    if err != nil {
-        log.Fatal(err)
-    }
-    pwd := password.Get_pwd("Test")
+    // TODO: Implement Arrowhead.
+    pwd := password.Get_pwd("Meth")
     fmt.Println(pwd)
-    // opening db
-    db, err := bolt.Open("data.db", 0600, nil)
-    if err != nil {
-        log.Fatal(err)
-    }
-    // creating the db
-//    db.Update(func (tx *bolt.Tx) error {
-//        _, err := tx.CreateBucket([]byte("Arrowhead"))
-//        if err != nil {
-//            return fmt.Errorf("create bucket %s",err)
-//        }
-//        return nil
-//    })
-    // updating db
-    db.Update(func (tx *bolt.Tx) error {
-        b := tx.Bucket([]byte("Arrowhead"))
-        err := b.Put([]byte("Username"), []byte("Password"))
-        return err
-    })
-    // getting value from db
-    db.View(func (tx *bolt.Tx) error {
-        b := tx.Bucket([]byte("Arrowhead"))
-        v := b.Get([]byte("Username"))
-        fmt.Printf("%s", v)
-        return nil
-    })
-    // closing db
-    defer db.Close()
 }
